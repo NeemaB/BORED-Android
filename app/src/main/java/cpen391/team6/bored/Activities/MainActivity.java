d7aa80d32996bdc2aa8089edccda4a21aa4aa237
Neema <commodornim@googlemail.com>
2017-03-27 22:55:41 -0700

    Add confirmation dialog for when user leaves create note page

--- app/src/main/java/cpen391/team6/bored/Activities/MainActivity.java
+++ app/src/main/java/cpen391/team6/bored/Activities/MainActivity.java
@@ -1,5 +1,6 @@
 package cpen391.team6.bored.Activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
@@ -7,10 +8,12 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -38,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
     private ListView mDrawerList;
     private CharSequence mTitle;
     private Menu mMenu;
+
+    private static int COURSE_NOTES_POSITION = 0;
+    private static int CREATE_NOTE_POSITION = 1;
+    private static int SETTINGS_POSITION = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
 
             fragmentTransaction.commit();
             mCurrentFragment = fragment;
-            
+
             mDrawerLayout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
                 @Override
                 public void onGlobalLayout() {
@@ -107,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
          /* Retrieve the previously selected position in the drawer and assign it to the newly created instance
           * of this activity, then update the drawer list UI
           */
-        }else{
+        } else {
             mCurrentPosition = savedInstanceState.getInt("current_position");
             mDrawerLayout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
                 @Override
@@ -117,9 +124,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
-
-
-
 
 
     }
@@ -223,85 +227,72 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
-    private void selectItem(int position) {
+    private void selectItem(final int position) {
 
         /* Create a new fragment and specify the view to show depending on which option is chosen */
-        Fragment fragment = null;
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        mCurrentPosition = position;
-        updateDrawerList();
+
 
         switch (position) {
 
             case 0:
 
-                /* First check to see if a fragment exists before we create a new one */
-                fragment = getFragmentManager()
-                        .findFragmentByTag(getString(R.string.course_notes_fragment_tag));
-
-                if (fragment == null)
-                    fragment = new CourseNotesFragment();
-
-                /* Replace the current fragment that is being displayed, provide it with a tag so we can
-                 * locate it in the future
-                 */
-
-                transaction.replace(R.id.content_frame,
-                        fragment,
-                        getString(R.string.course_notes_fragment_tag));
-
-                /* Ensure that the fragment is displayed in portrait mode */
-                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-                /* Actually make the transition */
-                transaction.commit();
-
-                mCurrentFragment = fragment;
-
-
-                /* Allow swipe activation of drawer
+                /* If we are on the create note page, inform the user that their unsaved changes will be lost,
+                *  if the user selects yes then we will transition to the new fragment, otherwise we will stay where
+                *  we are
                 */
-                unLockDrawer();
+                if (mCurrentPosition == CREATE_NOTE_POSITION) {
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(
+                            new ContextThemeWrapper(this, R.style.DialogTheme));
+
+                    builder.setTitle(getString(R.string.leave_fragment_title))
+                            .setMessage(R.string.leave_fragment_message)
+                            .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                    loadCourseNotesFragment();
+                                    mCurrentPosition = position;
+                                    updateDrawerList();
+                                    // Highlight the selected item, update the title, and close the drawer
+                                    mDrawerList.setItemChecked(position, true);
+                                    //setTitle(mDrawerTitles[position]);
+                                    dialog.dismiss();
+                                }
+                            })
+                            .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+
+                    AlertDialog confirmationDialog = builder.create();
+                    confirmationDialog.setCanceledOnTouchOutside(true);
+                    confirmationDialog.show();
+
+                    UI_Util.setDialogStyle(confirmationDialog, this);
+
+                    mDrawerLayout.closeDrawer(mDrawerList);
+                    return;
+
+                }
+
+                loadCourseNotesFragment();
+
 
                 break;
 
 
             case 1:
 
-            /* First check to see if a fragment exists before we create a new one */
-                fragment = getFragmentManager()
-                        .findFragmentByTag(getString(R.string.create_note_fragment_tag));
-
-                if (fragment == null)
-                    fragment = new CreateNoteFragment();
-
-                /* Replace the current fragment that is being displayed, provide it with a tag so we can
-                 * locate it in the future
-                 */
-
-                transaction.replace(R.id.content_frame,
-                        fragment,
-                        getString(R.string.create_note_fragment_tag));
-
-
-                /* Actually make the transition */
-                transaction.commit();
-
-                mCurrentFragment = fragment;
-
-                /* We don't want to interfere with the drawing space so disable gesture activation of the
-                * drawer layout
-                */
-                lockDrawer();
-
-                /* Ensure that the fragment is displayed in landscape mode */
-                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                loadCreateNoteFragment();
 
                 break;
-
-
         }
 
+        mCurrentPosition = position;
+        updateDrawerList();
         // Highlight the selected item, update the title, and close the drawer
         mDrawerList.setItemChecked(position, true);
         //setTitle(mDrawerTitles[position]);
@@ -311,11 +302,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle savedInstanceState){
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
 
         savedInstanceState.putInt("current_position", mCurrentPosition);
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         /* This does nothing, we only want to call the corresponding function in our fragment
@@ -357,7 +349,78 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void loadCourseNotesFragment() {
 
+        Fragment fragment = null;
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+        /* First check to see if a fragment exists before we create a new one */
+        fragment = getFragmentManager()
+                .findFragmentByTag(getString(R.string.course_notes_fragment_tag));
+
+        if (fragment == null)
+            fragment = new CourseNotesFragment();
+
+                /* Replace the current fragment that is being displayed, provide it with a tag so we can
+                 * locate it in the future
+                 */
+
+        transaction.replace(R.id.content_frame,
+                fragment,
+                getString(R.string.course_notes_fragment_tag));
+
+                /* Ensure that the fragment is displayed in portrait mode */
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+                /* Actually make the transition */
+        transaction.commit();
+
+        mCurrentFragment = fragment;
+
+
+                /* Allow swipe activation of drawer
+                */
+        unLockDrawer();
+    }
+
+    private void loadCreateNoteFragment() {
+
+        Fragment fragment = null;
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+        /* First check to see if a fragment exists before we create a new one */
+        fragment = getFragmentManager()
+                .findFragmentByTag(getString(R.string.create_note_fragment_tag));
+
+        if (fragment == null)
+            fragment = new CreateNoteFragment();
+
+        /* Replace the current fragment that is being displayed, provide it with a tag so we can
+         * locate it in the future
+         */
+        transaction.replace(R.id.content_frame,
+                fragment,
+                getString(R.string.create_note_fragment_tag));
+
+
+        /* Actually make the transition */
+        transaction.commit();
+
+        mCurrentFragment = fragment;
+
+        /* We don't want to interfere with the drawing space so disable gesture activation of the
+         * drawer layout
+         */
+        lockDrawer();
+
+        /* Ensure that the fragment is displayed in landscape mode */
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+    }
+
+    private void loadSettingsFragment() {
+
+    }
 
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
         @Override
