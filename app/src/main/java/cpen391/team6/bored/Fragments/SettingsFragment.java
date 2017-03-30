7119b1883917d6a22a98712e34038f0307b32feb
Corwin Shanner <corwin.shanner@gmail.com>
2017-03-29 17:02:15 -0700

    work in progress

--- app/src/main/java/cpen391/team6/bored/Fragments/SettingsFragment.java
+++ app/src/main/java/cpen391/team6/bored/Fragments/SettingsFragment.java
@@ -1,10 +1,14 @@
 package cpen391.team6.bored.Fragments;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.preference.Preference;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
 
 import cpen391.team6.bored.Activities.MainActivity;
+import cpen391.team6.bored.R;
 
 /**
  * Created by Corwin on 2017-03-28.
@@ -17,11 +21,38 @@ public class SettingsFragment extends PreferenceFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    public void onPreferenceClick(){
 
     }
+
+
+    public void onClassCodeClicked (View view)
+    {
+        // create a new AlertDialog Builder object
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        // set the message and the Title
+        builder.setTitle("Enter Class Code:");
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick( DialogInterface dialog, int id) {
+                // put any "No/cancel" response code here
+            }
+        });
+        builder.setPositiveButton ("OK", new DialogInterface.OnClickListener() {
+            public void onClick( DialogInterface dialog, int id) {
+                // put any "Yes" response code here
+            }
+        });
+
+
+        // make and show the dialog box
+        AlertDialog dialog = builder.create() ;
+        dialog.show();
+
+        // set the width and height of the dialog box
+        dialog.getWindow().setLayout(400, 200);
+    }
+
 
     @Override
     public void onStart() {
