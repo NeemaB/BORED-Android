b046b967ddb1825cb1677f89db12834198a071d2
Corwin Shanner <corwin.shanner@gmail.com>
2017-04-03 15:46:48 -0700

    added class code display in settings

--- app/src/main/java/cpen391/team6/bored/Fragments/SettingsFragment.java
+++ app/src/main/java/cpen391/team6/bored/Fragments/SettingsFragment.java
@@ -2,8 +2,9 @@ package cpen391.team6.bored.Fragments;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
 
 import cpen391.team6.bored.R;
 
@@ -11,8 +12,7 @@ import cpen391.team6.bored.R;
  * Created by Corwin on 2017-03-28.
  */
 
-public class SettingsFragment extends PreferenceFragment {
-
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -20,13 +20,25 @@ public class SettingsFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings_fragment);
 
+        SharedPreferences sp = getPreferenceScreen().getSharedPreferences();
+        EditTextPreference editTextPref = (EditTextPreference) findPreference("classCodePref");
+        editTextPref
+                .setSummary(sp.getString("classCodePref", ""));
+        sp.registerOnSharedPreferenceChangeListener(this);
     }
 
     public String getClassCode() {
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this.getActivity());
-        return sharedPref.getString("classCodePref", "");
+        SharedPreferences sp = getPreferenceScreen().getSharedPreferences();
+        EditTextPreference editTextPref = (EditTextPreference) findPreference("classCodePref");
+        return sp.getString("classCodePref", "");
     }
 
-
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference pref = findPreference(key);
+        if (pref instanceof EditTextPreference) {
+            EditTextPreference etp = (EditTextPreference) pref;
+            pref.setSummary(etp.getText());
+        }
+    }
 }
-
